755784197ebdf79598c0900b74b6711fd69e68d0
floki <flokitheprogrammer@gmail.com>
2021-12-08 01:30:49 +0500

    stopped default behavior of formsubmit and created JS logic for storing books as objects

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ const btnCancel = document.getElementById("cancel");
 const btnAdd = document.getElementById("addBook");
 const popup = document.querySelector(".bookPopup");
 
-formSubmit.addEventListener("submit", handleForm);
+formSubmit.addEventListener("submit", addBookToLibrary);
 
 btnCancel.addEventListener("click", removePopup);
 btnAdd.addEventListener("click", showPopup);
 
 function removePopup() {
+  formSubmit.reset();
   popup.style["display"] = "none";
 }
 
@@ -17,7 +18,35 @@ function showPopup() {
   popup.style["display"] = "block";
 }
 
-function handleForm(event) {
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(event) {
   event.preventDefault();
-  console.log("Hello");
+
+  //Get names submitted in the current form
+
+  let currentBook = document.getElementById("name").value;
+  let currentAuthor = document.getElementById("author").value;
+  let currentNumberOfPages = document.getElementById("number-of-pages").value;
+  let currentReadStatus = document.getElementById("read").checked;
+
+  //Creating a unique object with the given input and adding it to library
+
+  myLibrary.push(
+    new Book(
+      currentBook,
+      currentAuthor,
+      currentNumberOfPages,
+      currentReadStatus
+    )
+  );
+  removePopup();
+
 }
